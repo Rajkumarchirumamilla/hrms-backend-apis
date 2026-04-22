38909a47ced6343d1a8d5970225ac973b9a05a8e
gopi-c1 <Tayanoorugopi@sltechnocrats.net>
2026-04-22 14:41:39 +0530

    "port issue"

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use("/api/attendance", require("./routes/attendanceRoutes"));
 
 
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
